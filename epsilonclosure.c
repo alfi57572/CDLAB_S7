0068751196dbf43622757ff74e314f485637f0bc
ALFIYA MA <122954741+alfi57572@users.noreply.github.com>
2025-09-08 21:06:02 +0530

    Update epsilonclosure.c

--- epsilonclosure.c
+++ epsilonclosure.c
@@ -1,96 +1,50 @@
+
+
+
 #include<stdio.h>
 #include<string.h>
-char result[20][20], copy[3], states[20][20];
 
-void add_state(char a[3], int i) {
-
-  strcpy(result[i], a);
-
-}
+char result[20][20], current_state[3];
 
 void display(int n) {
-
-  int k = 0;
-
-  printf("\n  Epsilon closure of %s = { ", copy);
-
-  while (k < n) {
-
-    printf(" %s", result[k]);
-
-    k++;
-
-  }
-
-  printf(" }\n");
-
+    printf("\n  Epsilon closure of %s = {", current_state);
+    for(int i=0; i<n; i++) {
+        printf(" %s", result[i]);
+    }
+    printf(" }");
 }
 
 int main() {
-
-  FILE * INPUT;
-
-  INPUT = fopen("automata.txt", "r");
-
-  char state[3];
-
-  int end, i = 0, n, k = 0;
-
-  char state1[3], input[3], state2[3];
-
-  printf("\n Enter the no of states: ");
-
-  scanf("%d",&n);
-
-  printf("\n Enter the states  \n");
-
-  for (k = 0; k < n; k++) {
-
-    scanf("%s", states[k]);
-
-  }
-
-  for (k = 0; k < n; k++) {
-
-    i = 0;
-
-    strcpy(state, states[k]);
-
-    strcpy(copy, state);
-
-    add_state(state, i++);
-
-    while (1) {
-
-      end = fscanf(INPUT, "%s%s%s", state1, input, state2);
-
-      if (end == EOF) {
-
-        break;
-
-      }
-
-      if (strcmp(state, state1) == 0) {
-
-        if (strcmp(input, "e") == 0) {
-
-          add_state(state2, i++);
-
-          strcpy(state, state2);
-
-        }
-
-      }
-
+    FILE *file = fopen("automata.txt", "r");
+    int n;
+    char state[3], state1[3], input[3], state2[3];
+    
+    printf("Enter number of states: ");
+    scanf("%d", &n);
+    
+    char states[n][3];
+    printf("Enter state names: ");
+    for(int i=0; i<n; i++) {
+        scanf("%s", states[i]);
     }
-
-    display(i);
-
-    rewind(INPUT);
-
-  }
-
-  return 0;
-
-}     
-
+    
+    for(int i=0; i<n; i++) {
+        int count = 0;
+        strcpy(state, states[i]);
+        strcpy(current_state, state);
+        strcpy(result[count++], state);
+        
+        while(fscanf(file, "%s%s%s", state1, input, state2) != EOF) {
+            if(strcmp(state, state1)==0 && strcmp(input, "e")==0) {
+                strcpy(result[count++], state2);
+                strcpy(state, state2);
+            }
+        }
+        
+        display(count);
+        rewind(file);
+    }
+    
+    fclose(file);
+    return 0;
+}
